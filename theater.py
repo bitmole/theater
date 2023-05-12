7ccca20e4693f96be9362289696add2d36e3bc6e
bitmole <robert@idea-loop.com>
2023-05-12 15:32:37 -0600

    Difference in attendance.

--- theater.py
+++ theater.py
@@ -27,4 +27,39 @@
 # by Felleisen, Findler, Flatt, and Krishnamurthi.  pg. 60.
 
 
+def attendance_at(price):
+    """
+    Calculates number of attendees at given price
+    """
+    return 120 + diff_attendance(price)
 
+def diff_attendance(price):
+    """Calculates difference in attendence at given price
+        according to following function:
+
+        diff_price      | 10 | 20 | 30 | 40
+        diff_attendance | 15 | 30 | 45 | 60
+        
+        This denotes linear f(x) = 1.5.x
+                            f(x) = 3/2 x
+                            f(x) = (3 * x) / 2
+
+    :price: ticket price
+    :returns: difference in attendance as compared to attendance at base price
+
+    """
+    return (3 * diff_price(price)) // 2
+
+def diff_price(price):
+    """
+    Calculates difference between base price ($5.00) and given price
+
+    :price: ticket price
+    :returns: price difference *in cents*
+    """
+    diff_cents = round(price * 100)
+    return 500 - diff_cents
+
+assert attendance_at(5.00) == 120
+assert attendance_at(5.10) == 105
+assert attendance_at(4.90) == 135
